d812b875b8a1a1245858fe7325a5d8dbcf27d02c
zeineb-saadouli <saadoulizeineb1@gmail.com>
2025-01-07 15:11:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+ 
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/TaskContext.js
@@ -0,0 +1,21 @@
+import React,{createContext,useEffect,useState} from "react";
+export const TaskContext=createContext();
+export const TaskProvidor=({children})=>{
+    const [tasks,setTasks]=useState([]);
+    useEffect(()=>{
+const storedTasks=JSON.parse(localStorage.getItem('tasks'))|| [];
+setTasks(storedTasks);
+    },[]);
+    useEffect(()=>{
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    },[tasks]);
+    //ajout d'une tache
+    const addTask=(task)=> setTasks([...tasks,task]);
+    //effacer une tache
+    const deleteTask=(id) => setTasks(tasks.filter((task)=>task.id!==id));
+    // la mise à jour des taches
+    const updateTask=(updatedTask,id)=> setTasks(tasks.map((task)=>task.id===updatedTask.id?updatedTask:task))  
+return(
+    <TaskContext.Provider value={{tasks,addTask,deleteTask,updateTask}}>{children}</TaskContext.Provider>
+)   
+}
